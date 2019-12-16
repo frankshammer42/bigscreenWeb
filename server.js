45d2058aece9fbc56194c2bbe4b3bb358974e00a
frankshammer42 <ra2901@nyu.edu>
2019-12-16 05:18:01 -0500

    Before qunce

--- server.js
+++ server.js
@@ -24,13 +24,25 @@ let number_of_client_connections = 0;
 let target_number = 10;
 
 
+let total_staircase_lights = 6;
+
 //Progress Tracking
 let progress = 0;
 let cut_progress = 0;
+let shadow_curtain_progress = 0;
+let staircase_screen_index_progress = 0;
+let staircase_lights_progress = 0;
+let staircase_sockets_progress = 0;
+let socket_sent_out_numbers = 0;
+let connections_per_screen = 0;
+
 // progress
 // 0 - wait for connection
 // 1 - start removing blocks
 // 2 - train cut
+// 3 - start screen
+// 4 - start staircase
+// 5 - shadow scene
 // 100 - waiting transition page
 
 
@@ -39,23 +51,42 @@ web_clients_connection.on('connection', (socket) => {
     console.log("Get connection from web client");
 
     switch (progress) {
+        case 0:
+            unity_client_connection.emit("intro_client_connect");
+            break;
         case 1:
             console.log("add that socket into the intro scene");
             socket.emit("intro_scene"); //only add to that socket
             break;
         case 2:
             console.log("add that socket into the cut scene");
-            cut_progress += 1;
             let segmentIndex = (cut_progress/num_cuts_per_segment) | 0;
             let cutIndex = cut_progress % num_cuts_per_segment;
-            // if (segmentIndex < block_cut_points.length){
-            let cut = block_cut_points[segmentIndex];
-            let client_cut = cut[cutIndex];
-            // let cut = cut_points[i];
-            // let socket_id = connected_sockets_ids[cut_progress];
-            // console.log("Send cut point", client_cut, "to", socket_id);
-            // web_clients_connection.to(socket_id).emit("cut_scene", {"cut":cut, "index":i, "cut_bound":cut_bound});
-            socket.emit("cut_scene", {"cut":client_cut, "index":cut_progress, "cut_bound":cut_bound, "segment_index": segmentIndex});
+            if (segmentIndex < block_cut_points.length) {
+                let cut = block_cut_points[segmentIndex];
+                let client_cut = cut[cutIndex];
+                socket.emit("cut_scene", {
+                    "cut": client_cut,
+                    "index": cut_progress,
+                    "cut_bound": cut_bound,
+                    "segment_index": segmentIndex
+                });
+                cut_progress += 1;
+            }
+            break;
+        case 3:
+            console.log("add that socket to screen scene");
+            socket.emit("screen_city_scene");
+            break;
+        case 4:
+            console.log("add that socket to staircase scene");
+            socket.emit("staircase_scene", {"screen_index":staircase_screen_index_progress, "light_index": staircase_lights_progress});
+            staircase_lights_progress += 1;
+            break;
+        case 5:
+            console.log("add that socket to shadow scene");
+            socket.emit("shadow_scene", {"index":shadow_curtain_progress});
+            shadow_curtain_progress += 1;
             break;
         case 100:
             web_clients_connection.emit("waiting_page");
@@ -66,7 +97,6 @@ web_clients_connection.on('connection', (socket) => {
 
 
     // if (at_beginning){
-    //     unity_client_connection.emit("intro_client_connect");
     // }
     // number_of_client_connections += 1;
     // if (number_of_client_connections === target_number){
@@ -75,11 +105,7 @@ web_clients_connection.on('connection', (socket) => {
     //     at_beginning = false;
     // }
     socket.on('disconnect', function(){
-        // number_of_client_connections -= 1;
         console.log('got disconnected');
-        // if (at_beginning){
-        //     unity_client_connection.emit("intro_client_disconnect");
-        // }
     });
 
     socket.on('client_erase_block', function () {
@@ -104,6 +130,32 @@ web_clients_connection.on('connection', (socket) => {
         let cut_index = data;
         console.log(cut_index);
         unity_client_connection.emit("cut", cut_index);
+    });
+    
+    socket.on('client_turnoff_screen', function (data) {
+        console.log("Client turns off screen");
+        unity_client_connection.emit("turnoff_screen");
+    });
+
+    socket.on('client_moveup_curtain', function (data) {
+        console.log("client moves up curtain");
+        let curtain_index = data.index;
+        let curtain_target = data.progress;
+        console.log(data);
+        console.log(curtain_index);
+        console.log(curtain_target);
+        unity_client_connection.emit("curtain_move_up", curtain_index, curtain_target);
+    });
+
+    socket.on('client_turnon_light', function (data) {
+        console.log("client turns on light");
+        let screen_index = data.screen_index;
+        let light_index = data.light_index;
+        let light_target = data.progress;
+        console.log(screen_index);
+        console.log(light_index);
+        console.log(light_target);
+        unity_client_connection.emit("light_turn_on", screen_index, light_index, light_target);
     });
 });
 
@@ -172,19 +224,16 @@ unity_client_connection.on('connection', (socket) => {
         let connected_sockets_ids = Object.keys(web_clients_connection.connected);
         let num_connected_sockets = connected_sockets_ids.length;
         for (let i=0; i<num_connected_sockets; i++){
-            // let cut = cut_points.pop();
             let segmentIndex = (i/num_cuts_per_segment) | 0;
             let cutIndex = i % num_cuts_per_segment;
-            // if (segmentIndex < block_cut_points.length){
-            let cut = block_cut_points[segmentIndex];
-            let client_cut = cut[cutIndex];
-            // let cut = cut_points[i];
-            let socket_id = connected_sockets_ids[i];
-            console.log("Send cut point", client_cut, "to", socket_id);
-            // web_clients_connection.to(socket_id).emit("cut_scene", {"cut":cut, "index":i, "cut_bound":cut_bound});
-            web_clients_connection.to(socket_id).emit("cut_scene", {"cut":client_cut, "index":i, "cut_bound":cut_bound, "segment_index": segmentIndex});
-            cut_progress += 1;
-            // }
+            if (segmentIndex < block_cut_points.length){
+                let cut = block_cut_points[segmentIndex];
+                let client_cut = cut[cutIndex];
+                let socket_id = connected_sockets_ids[i];
+                console.log("Send cut point", client_cut, "to", socket_id);
+                web_clients_connection.to(socket_id).emit("cut_scene", {"cut":client_cut, "index":i, "cut_bound":cut_bound, "segment_index": segmentIndex});
+                cut_progress += 1;
+            }
         }
         // web_clients_connection.emit("cut_scene");
     });
@@ -193,6 +242,57 @@ unity_client_connection.on('connection', (socket) => {
         console.log("cut pieces should fall");
         web_clients_connection.emit("cut_piece_fall");
     });
+
+    socket.on('start_screen', function (data) {
+        console.log("Start screen city interaction");
+        progress = 3;
+        web_clients_connection.emit("screen_city_scene");
+    });
+
+    socket.on('start_shadow', function (data) {
+        console.log("Start shadow scene");
+        progress = 5;
+        let connected_sockets_ids = Object.keys(web_clients_connection.connected);
+        let num_connected_sockets = connected_sockets_ids.length;
+        for (let i=0; i<num_connected_sockets; i++){
+            let socket_id = connected_sockets_ids[i];
+            console.log("socket id is", socket_id);
+            web_clients_connection.to(socket_id).emit("shadow_scene", {"index":i});
+            shadow_curtain_progress += 1;
+        }
+    });
+
+    socket.on('start_staircase', function (data) {
+        progress = 4;
+        console.log("Start staircase scene for", data);
+        let current_screen_index = data;
+        current_screen_index = 2 - current_screen_index;
+        let connected_sockets_ids = Object.keys(web_clients_connection.connected);
+        let num_connected_sockets = connected_sockets_ids.length;
+        if (current_screen_index === 0){
+            console.log("------");
+            console.log(num_connected_sockets);
+            connections_per_screen = num_connected_sockets/3;
+        }
+        // connections_per_screen = total_staircase_lights/3;
+        staircase_screen_index_progress = current_screen_index;
+        // staircase_screen_index_progress  = current_screen_index;
+        let start_index = current_screen_index*connections_per_screen;
+        console.log("Current start index is", start_index);
+        for (let i=start_index; i<start_index+connections_per_screen; i++){
+            if (i < connected_sockets_ids.length){
+                socket_sent_out_numbers = i;
+                let socket_id = connected_sockets_ids[i];
+                let light_index = i - start_index;
+                console.log("light index is", light_index);
+                console.log("socket id is", socket_id);
+                web_clients_connection.to(socket_id).emit("staircase_scene", {"screen_index":staircase_screen_index_progress, "light_index": light_index});
+                staircase_lights_progress = light_index;
+            }
+        }
+        staircase_lights_progress = 0;
+    })
+
 });
 
 app.get('/', function(req, res) {
@@ -237,13 +337,47 @@ app.get('/batch_draw_cut_lines', function (req, res) {
 
 app.get('/train_cut', function (req, res) {
     unity_client_connection.emit("train_cut");
-    web_clients_connection.emit("waiting_page");
+    setTimeout(sendWaitingPage, 2500);
+    // web_clients_connection.emit("waiting_page");
     console.log("train explodes");
     progress = 100; // Go back to the waiting page
     res.send("start train cut");
 });
 
+app.get('/batch_turnoff_screen', function (req, res) {
+    unity_client_connection.emit("batch_turnoff_screen");
+    // web_clients_connection.emit("waiting_page");
+    progress = 100; // Go back to the waiting page
+    res.send("start erase screens");
+});
+
+app.get('/batch_light_on_0', function (req, res) {
+    unity_client_connection.emit("batch_light_on_0");
+    progress = 100;
+    res.send("start batch turn on lights for 0 staircase");
+});
+
+app.get('/batch_light_on_1', function (req, res) {
+    unity_client_connection.emit("batch_light_on_1");
+    res.send("start batch turn on lights for 1 staircase");
+});
+
+app.get('/batch_light_on_2', function (req, res) {
+    unity_client_connection.emit("batch_light_on_2");
+    res.send("start batch turn on lights for 2 staircase");
+});
+
+
+app.get('/batch_curtain', function (req, res) {
+    unity_client_connection.emit("batch_curtain");
+    progress = 100;
+    res.send("start move up curtains");
+});
 
 server.listen(process.env.PORT || 8000, function() {
     console.log('Server is running on', server.address().port);
 });
+
+function sendWaitingPage(){
+    web_clients_connection.emit("waiting_page");
+}
